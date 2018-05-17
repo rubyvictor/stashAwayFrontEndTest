cb5323a4e358b21a04decac8c554af47463d148d
Victor Lee <myleevictor@gmail.com>
2018-05-17 22:21:52 +0800

    completed Composition component css

--- src/component/Composition.js
+++ src/component/Composition.js
@@ -1,38 +1,38 @@
 import React from "react";
+import "./Composition.css";
 
 const Composition = props => {
   return (
-    <div>
-      Composition component
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <p>{props.USD}</p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>{props.SGD}</p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>{props.EUR}</p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>{props.JPY}</p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>{props.Others}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="composition_wrapper">
+      <p id="composition_title">Portfolio Composition</p>
+      <p id="composition_subtitle">Global Currency Exposure</p>
+      <div className="composition_container">
+        <div className="usd_container">
+          <div className="composition_usd_box" />
+          <div className="composition_usd_label">USD</div>
+          <div className="composition_usd">{props.USD}</div>
+        </div>
+        <div className="sgd_container">
+          <div className="composition_sgd_box" />
+          <div className="composition_sgd_label">SGD</div>
+          <div className="composition_sgd">{props.SGD}</div>
+        </div>
+        <div className="eur_container">
+          <div className="composition_eur_box" />
+          <div className="composition_eur_label">EUR</div>
+          <div className="composition_eur">{props.EUR}</div>
+        </div>
+        <div className="jpy_container">
+          <div className="composition_jpy_box" />
+          <div className="composition_jpy_label">JPY</div>
+          <div className="composition_jpy">{props.JPY}</div>
+        </div>
+        <div className="others_container">
+          <div className="composition_others_box" />
+          <div className="composition_others_label">Others</div>
+          <div className="composition_others">{props.Others}</div>
+        </div>
+      </div>
     </div>
   );
 };
